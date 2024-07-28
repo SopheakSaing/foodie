0f4f9fb483422560e836105e23a6b7bdb19261d9
Pheak02 <saingsopheak02@gmail.com>
2024-07-29 00:07:22 +0700

    can use onMounted & able to req

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,9 +5,13 @@
       class="rounded border-2 border-gray-200 w-full"
       placeholder="Search for Meal"
     />
-    <div class="flex gap-1 items-center justify-center mt-2 ">
+    <div class="flex gap-1 items-center justify-center mt-2">
       <!-- <pre>{{letters}}</pre> -->
-      <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        :key="letter"
+      >
         {{ letter }}
       </router-link>
     </div>
@@ -16,11 +20,17 @@
 
 
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted } from "vue";
 import store from "../store";
+import axiosClient from "../axiosClient.js";
 
 const meals = computed(() => store.state.meals); //return meal
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+onMounted(async () => {
+  const response = await axiosClient.get("/list.php?c=list");
+  console.log(response.data);
+});
 </script>
 
 
