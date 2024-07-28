ca98064d3d7d59571d61faa8e8ff378c021389f8
Pheak02 <saingsopheak02@gmail.com>
2024-07-28 16:03:03 +0700

    create home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="flex p-8 flex-col items-center justify-center">
+  <div class="flex p-8 flex-col">
     <input
       type="text"
       class="rounded border-2 border-gray-200 w-full"
       placeholder="Search for Meal"
     />
-    <div class="flex gap-1">
+    <div class="flex gap-1 items-center justify-center mt-2 ">
       <!-- <pre>{{letters}}</pre> -->
-      <router-link to="/" v-for="letter of letters" :key="letter">
+      <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
         {{ letter }}
       </router-link>
     </div>
